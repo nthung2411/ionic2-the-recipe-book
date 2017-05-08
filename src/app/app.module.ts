5c2249bcfb493d03f9de1570485dfa47ce8ecb21
Robert Hung Nguyen <nthung2411@gmail.com>
2017-05-08 15:06:31 +0700

    add basic pages:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicErrorHandler, IonicModule, Tabs } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { ShoppingList } from "../pages/shopping-list/shopping-list";
+import { Recipes } from "../pages/recipes/recipes";
+import { Recipe } from "../pages/recipe/recipe";
+import { EditRecipe } from "../pages/edit-recipe/edit-recipe";
+import { TabsPage } from "../pages/tabs/tabs";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    TabsPage,
+    ShoppingList,
+    Recipes,
+    Recipe,
+    EditRecipe
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    TabsPage,
+    ShoppingList,
+    Recipes,
+    Recipe,
+    EditRecipe
   ],
   providers: [
     StatusBar,
---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ShoppingList } from "../shopping-list/shopping-list";
+import { Recipes } from "../recipes/recipes";
+
+@Component({
+  selector: 'page-tabs',
+  templateUrl: 'tabs.html',
+})
+export class TabsPage {
+  shoppingListPage = ShoppingList;
+  recipesPage = Recipes;
+}
